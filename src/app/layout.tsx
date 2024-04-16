2ee5c444888524058c3018295894663227839b94
Ricardo Castro <soyricardodev@proton.me>
2024-04-16 15:26:33 -0400

    add sidenav and tw indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { CSPostHogProvider } from "./_analytics/provider";
+import { SideNav } from "./_components/side-nav";
+import { TailwindIndicator } from "~/components/tailwind-indicator";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -33,14 +35,20 @@ export default function RootLayout({
         <html lang="es">
           <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           <body
-            className={`font-sans ${inter.variable} flex flex-col gap-4 bg-neutral-100 text-neutral-900`}
+            className={`font-sans ${inter.variable}bg-white`}
+            style={{ scrollbarColor: "rgb(185, 202, 211) rgb(247, 249, 249)" }}
           >
-            <div className="grid h-screen grid-rows-[auto,1fr]">
-              <TopNav />
-              <main className="mx-auto mt-9 w-full max-w-screen-xl overflow-y-scroll">
-                {children}
-              </main>
+            <div className="css-175 pointer-events-none [flex:1_1_0%]">
+              <div className="css-175 pointer-events-none [flex:1_1_0%]">
+                <div className="css-175 pointer-events-auto z-0 min-h-dvh w-full flex-row">
+                  <SideNav />
+                  <main className="css-175 w-full shrink grow items-start overflow-y-scroll">
+                    {children}
+                  </main>
+                </div>
+              </div>
             </div>
+            <TailwindIndicator />
           </body>
         </html>
       </CSPostHogProvider>
